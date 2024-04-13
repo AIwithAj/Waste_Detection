da30a40ea2e1a6be10b881aaeb240b7916596a4e
Ajay <dancerworld60@gmail.com>
2024-04-13 13:39:23 +0530

    folder Structure and setup is done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Ajay-Mehta',
+    author_email= '9110161299@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
